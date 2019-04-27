1fdacf50c1e29556c4d53843201a3b1c1143aac0
Shante Austin <shante.austin19@gmail.com>
2019-04-27 01:01:16 -0400

    Updates

---
+++ precache-manifest.95ad8a965cc67c02f9dbe5ee4c5e1b7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58802f377376db7484b5",
+    "url": "/React-Random-Quote-Generator/static/css/main.50fdb88a.chunk.css"
+  },
+  {
+    "revision": "58802f377376db7484b5",
+    "url": "/React-Random-Quote-Generator/static/js/main.4fda638e.chunk.js"
+  },
+  {
+    "revision": "37e52eb3f737fd376dae",
+    "url": "/React-Random-Quote-Generator/static/js/runtime~main.721657f8.js"
+  },
+  {
+    "revision": "b33ccf37b93ef91e4a68",
+    "url": "/React-Random-Quote-Generator/static/js/2.1e3ffc9b.chunk.js"
+  },
+  {
+    "revision": "614fb0f4e17bf672a4686a7a26253d13",
+    "url": "/React-Random-Quote-Generator/index.html"
+  }
+];
