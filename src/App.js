477aaf0894222a5e323f2758f6d6a4fee3c8af73
Shante Austin <shante.austin19@gmail.com>
2019-04-27 00:46:11 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import { Card } from "react-materialize";
 
 class App extends Component {
+  state = {
+    content: "",
+    link: "",
+    title: "",
+
+    hasQuote: false
+  };
+
+  getQuote = (link, title) => {
+    axios
+      .get(
+        `https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&timestamp=${new Date().getTime()}`
+      )
+      .then(response => {
+        const { title, content, link } = response.data[0];
+        this.setState(() => ({
+          title,
+          content,
+          link,
+          hasQuote: true
+        }));
+      })
+      .catch(err => {
+        console.log(`${err} whilst contacting the quote API.`);
+      });
+    console.log(link, title);
+  };
+
+  shareOnTwitter = (link, title) => {
+    const url = { link };
+    const text = { title };
+    window.open(
+      "http://twitter.com/share?url=" +
+        encodeURIComponent(url) +
+        "&text=" +
+        encodeURIComponent(text),
+      "",
+      "left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0"
+    );
+  };
+
   render() {
+    const { hasQuote } = this.state;
+    const { content } = this.state;
+    const { link } = this.state;
+    const { title } = this.state;
+    const str = content.toString();
+    const str_title = title;
+    const str_link = link;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {hasQuote === true ? (
+            <div>
+              <h3 className="titleQuote">Your Daily Quote</h3>
+              <Card className="cardType">
+                <div className="quoteContainer animated fadeIn">
+                  <h4 className="quoteText">{str.replace(/<[^>]*>/g, "")}</h4>{" "}
+                  <p className="quote-author">-{str_title}</p>
+                </div>
+              </Card>{" "}
+            </div>
+          ) : (
+            <img src="images/EVERYTHING.png" />
+          )}
+          <button id="quoteButton" onClick={this.getQuote}>
+            New Quote
+          </button>
         </header>
       </div>
     );
